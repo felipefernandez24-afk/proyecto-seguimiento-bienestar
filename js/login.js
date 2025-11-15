9c648748afb821a6aac5f270138ba504065868ff
Felipe Fernandez <felipe.fernandez.24@alumnos.uda.cl>
2025-11-15 17:21:09 -0300

    hecho primeros pasos de localStorage, almacenado datos de login

--- js/login.js
+++ js/login.js
@@ -10,8 +10,16 @@
         alert("Debes agregar datos al formulario", "danger");
     }
     else{
-        event.preventDefault()
-        //deberia aqui ir lo que se guarden los datos y mostrar su inicio de sesion
+        event.preventDefault();
+            const email = form.querySelector('#formGroupExampleInput').value.trim(); // Obtener los datos del formulario
+            const password = form.querySelector('#formGroupExampleInput2').value.trim(); // Obtener los datos del formulario
+
+            if (email && password) {// Validar que los campos no estén vacíos
+                localStorage.setItem("usuario", JSON.stringify({ email: email, password: password })); // Guardar el email y la contraseña en localStorage
+                window.location.href = "index.html"; // O redirigir a cualquier otra página
+            } else {
+                alert("Por favor, completa todos los campos."); // Por si acaso
+            }
     }
     form.classList.add('was-validated')
     }, false)
